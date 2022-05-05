e4b533a0da6c8b9a35e23744252e6069bbd8b3fd
myoungjinkang <myoungjinkang@kyobobook.com>
2022-05-05 14:04:17 +0900

    2022/05/05 Initial commit

---
+++ src/main/java/sk/snsTest/SnsTestApplication.java
@@ -0,0 +1,13 @@
+package sk.snsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsTestApplication.class, args);
+	}
+
+}
